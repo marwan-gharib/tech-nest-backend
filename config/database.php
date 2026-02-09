ba3485b2f96d956f2c676241b7dd1750cb2697a2
MarwanGhareeb <marwanghareeb146@gmail.com>
2026-02-09 18:58:18 +0200

    fix bug

--- config/database.php
+++ config/database.php
@@ -1,6 +1,10 @@
 <?php
 // Set the content type to JSON
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 
 $db_host = 'localhost';
 $db_name = 'ecommerce_db';
